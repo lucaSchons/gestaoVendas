52b44f81470160a66afa01a487643383f0583795
Lucas Schons <44869341+lucaSchons@users.noreply.github.com>
2022-06-06 19:06:37 -0300

    commit geral

---
+++ src/gestaovendas/GestaoVendas.java
@@ -0,0 +1,17 @@
+
+package gestaovendas;
+
+/**
+ *
+ * @author lucas
+ */
+public class GestaoVendas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
